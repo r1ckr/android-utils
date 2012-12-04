39c0597b74dcdd6d80fe40d8f3fdef2516ad0092
Richard Pablo <richardpablop@gmail.com>
2012-12-04 20:02:07 +0100

    Se agrega Notifications Example

---
+++ Notification Example/src/com/richard/notificationexample/MainActivity.java
@@ -0,0 +1,84 @@
+package com.richard.notificationexample;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	
+	public static final int NOTIFICATION_ID = 0;
+	
+	NotificationManager mNotificationManager;
+	NotificationCompat.Builder mBuilder;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mNotificationManager =
+			    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		/*Si la notificacion se quiere cancelar al entrar en el Activity se hace así y 
+		 * se quita el autocancel en la construcción, de esta manera es mejor */
+		mNotificationManager.cancel(NOTIFICATION_ID);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	public void onClick(View v){
+		switch (v.getId()) {
+		case R.id.btn_add_simple_notification:
+			buildNotification("My Simple notification", "Hello World!");
+			// The Id allows you to update the notification later on.
+			mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+			break;
+		case R.id.btn_update_notification:
+			buildNotification("My Simple notification", "Notification updated!!");
+			// Here we use the Id to update the notification
+			mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private void buildNotification(String title, String content){
+		
+		
+		mBuilder = new NotificationCompat.Builder(this)
+		        .setSmallIcon(R.drawable.ic_launcher)
+		        .setContentTitle(title)
+		        .setContentText(content);
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(this, MainActivity.class);
+
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		// Adds the back stack for the Intent (but not the Intent itself)
+		stackBuilder.addParentStack(MainActivity.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_UPDATE_CURRENT
+		        );
+		mBuilder.setContentIntent(resultPendingIntent);
+		//Si se quiere cancelar cuando el usuario haga clic en ella se setea setAutoCancel a true
+		//mBuilder.setAutoCancel(true);
+	}
+	
+}
