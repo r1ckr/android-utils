5bf849ccd4b669359a9a595b3519bd976a777a1d
Richard Pablo <richardpablop@gmail.com>
2012-12-05 18:20:06 +0100

    Se crea un selector de API para lanzar Notifications.

--- Notification Example/gen/com/richard/notificationexample/R.java
+++ Notification Example/gen/com/richard/notificationexample/R.java
@@ -14,9 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btn_add_old_notification=0x7f070001;
+        public static final int btn_add_selected_notification=0x7f070003;
         public static final int btn_add_simple_notification=0x7f070000;
-        public static final int btn_update_notification=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int btn_update_notification=0x7f070002;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- Notification Example/src/com/richard/notificationexample/MainActivity.java
+++ Notification Example/src/com/richard/notificationexample/MainActivity.java
@@ -1,10 +1,12 @@
 package com.richard.notificationexample;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
@@ -17,6 +19,7 @@ public class MainActivity extends Activity {
 	
 	NotificationManager mNotificationManager;
 	NotificationCompat.Builder mBuilder;
+	Notification.Builder mBuilderHoney;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,14 +41,26 @@ public class MainActivity extends Activity {
 	public void onClick(View v){
 		switch (v.getId()) {
 		case R.id.btn_add_simple_notification:
+			
+			
 			buildNotification("My Simple notification", "Hello World!");
 			// The Id allows you to update the notification later on.
-			mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+			
 			break;
 		case R.id.btn_update_notification:
 			buildNotification("My Simple notification", "Notification updated!!");
 			// Here we use the Id to update the notification
-			mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+			break;
+		case R.id.btn_add_old_notification:
+			buildOldNotification("My Simple notification", "Hi! this is an old notification!!");
+			// Here we use the Id to update the notification
+			break;
+		case R.id.btn_add_selected_notification:
+			if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB){
+				buildOldNotification("My Simple notification", "Hi! this is an old notification!!");
+			}else{
+				buildNotificationForHoney("My Simple notification", "Hi! this is an old notification only for Honeycomb and beyond versions!!");
+			}
 			break;
 		default:
 			break;
@@ -53,7 +68,6 @@ public class MainActivity extends Activity {
 	}
 	
 	private void buildNotification(String title, String content){
-		
 		
 		mBuilder = new NotificationCompat.Builder(this)
 		        .setSmallIcon(R.drawable.ic_launcher)
@@ -79,6 +93,51 @@ public class MainActivity extends Activity {
 		mBuilder.setContentIntent(resultPendingIntent);
 		//Si se quiere cancelar cuando el usuario haga clic en ella se setea setAutoCancel a true
 		//mBuilder.setAutoCancel(true);
+		mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+	}
+	
+	private void buildNotificationForHoney(String title, String content){
+		
+		mBuilderHoney = new Notification.Builder(this)
+		        .setSmallIcon(R.drawable.ic_launcher)
+		        .setContentTitle(title)
+		        .setContentText(content);
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(this, MainActivity.class);
+
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		android.app.TaskStackBuilder stackBuilder = android.app.TaskStackBuilder.create(this);
+		// Adds the back stack for the Intent (but not the Intent itself)
+		stackBuilder.addParentStack(MainActivity.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_UPDATE_CURRENT
+		        );
+		mBuilderHoney.setContentIntent(resultPendingIntent);
+		//Si se quiere cancelar cuando el usuario haga clic en ella se setea setAutoCancel a true
+		//mBuilder.setAutoCancel(true);
+		mNotificationManager.notify(NOTIFICATION_ID, mBuilderHoney.build());
+	}
+
+	@SuppressWarnings("deprecation")
+	private void buildOldNotification(String title, String content){
+
+        Notification notification = new Notification(R.drawable.ic_launcher,
+        		content, System
+                        .currentTimeMillis());
+        // The PendingIntent will launch activity if the user selects this
+        // notification
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, new Intent(this, MainActivity.class), 0);
+        notification.setLatestEventInfo(this, title, content,
+                contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, notification);
 	}
 	
 }
